034e53298110b709b82ed5aab66763a2c03611e9
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-03-08 09:20:07 -0500

    Cleanup __init__.py imports

--- gutils/__init__.py
+++ gutils/__init__.py
@@ -4,10 +4,6 @@ This package imports core.py into its global namespace. See help(gutils.core)
 for documentation on globally defined functions.
 """
 
-import gutils.colorize as colorize
+from gutils import colorize, debug, io, logging, xdg
 from gutils.core import *
-import gutils.debug as debug
-import gutils.io as io
-import gutils.logging as logging
 from gutils.subprocess import create_pidfile
-import gutils.xdg as xdg
