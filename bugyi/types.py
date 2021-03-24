b15d363e913605fb718a5178e644e65c87d27862
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-03-24 16:46:19 -0400

    Add Final to types.py

--- bugyi/types.py
+++ bugyi/types.py
@@ -1,6 +1,9 @@
 from collections import defaultdict
 from pathlib import Path
-from typing import Any, Union
+from typing import Any, Type, TypeVar, Union
+
+
+_T = TypeVar("_T")
 
 
 # The below 'typing' module types are imported from this module by other
@@ -8,16 +11,25 @@ from typing import Any, Union
 #
 # Used to maintain Python<=3.7 compatibility.
 try:
-    from typing import Literal, Protocol  # pylint: disable=unused-import
+    from typing import (  # pylint: disable=unused-import
+        Final,
+        Literal,
+        Protocol,
+    )
 except ImportError:
     try:
-        from typing_extensions import Literal, Protocol  # type: ignore
+        from typing_extension import Final, Literal, Protocol  # type: ignore
     except ImportError:
+
+        class _FinalMock(type):
+            def __getitem__(cls, key: Type[_T]) -> Type[_T]:
+                return key
 
         class _ProtocolMock(type):
             def __getitem__(cls, _key: Any) -> "_ProtocolMock":
                 return cls
 
+        Final = _FinalMock("Final", (object,), {})  # type: ignore
         Literal = defaultdict(lambda: str)  # type: ignore
         Protocol = _ProtocolMock("Protocol", (object,), {})  # type: ignore
 
