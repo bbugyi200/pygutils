7a430593f4581bcff0e1e3c290ec2ad56a0ced87
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-03-24 11:39:24 -0400

    Add Literal to types.py

--- bugyi/types.py
+++ bugyi/types.py
@@ -2,14 +2,11 @@ from pathlib import Path
 from typing import Union
 
 
-# 'Protocol' is imported from this module by other modules/scripts.
+# These types are imported from this module by other modules/scripts.
 try:
-    from typing import Protocol  # pylint: disable=unused-import
+    from typing import Literal, Protocol  # pylint: disable=unused-import
 except ImportError:
-    try:
-        from typing_extensions import Protocol  # type: ignore
-    except ImportError:
-        Protocol = object  # type: ignore
+    from typing_extensions import Literal, Protocol  # type: ignore
 
 
 PathLike = Union[str, Path]
