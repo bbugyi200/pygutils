6dfb0f78f25da9a1c01f455d10db59e10a9c733a
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-03-28 12:18:33 -0400

    Add some explanatory comments to __init__.py

--- bugyi/__init__.py
+++ bugyi/__init__.py
@@ -4,6 +4,8 @@
 # import <NAME>` instead.
 from bugyi import colors, debug, io, logging, xdg
 
+# For accessing core functions/classes via `from bugyi import <NAME>`.
+#
 # DEPRECIATED: Use 'from bugyi.core import <NAME>' instead.
 from bugyi.core import (
     ArgumentParser,
@@ -17,12 +19,18 @@ from bugyi.core import (
     signal,
 )
 
+# For accessing tools.py functions via `from bugyi import <TOOL>`.
+#
 # DEPRECIATED: Import directly from the 'bugyi.tools' module instead.
 from bugyi.tools import notify, xkey, xtype
 
+# For accessing subprocess functions/classes via `from bugyi import <NAME>`.
+#
 # DEPRECIATED: Use 'from bugyi import subprocess as bsp' instead.
 from bugyi.subprocess import create_pidfile, StillAliveException
 
 
+# For accessing the colors modules via `from bugyi import colorize`.
+#
 # DEPRECIATED: Use `from bugyi import colors` instead.
 colorize = colors
