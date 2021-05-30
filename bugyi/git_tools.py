dcddb16242d90fd5b3630c73d7ca902793e6c768
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-05-30 11:32:48 -0400

    Fix git_tools.py mypy errors

--- bugyi/git_tools.py
+++ bugyi/git_tools.py
@@ -2,7 +2,7 @@ import sys
 from typing import Iterable, List, NamedTuple
 
 from . import subprocess as bsp
-from .errors import BResult
+from .errors import BResult, BErr
 from .result import Err, Ok
 
 
@@ -15,7 +15,7 @@ def top_level_dir(cwd: str = None) -> BResult[str]:
         ["git", "rev-parse", "--show-toplevel"], cwd=cwd
     )
     if isinstance(out_err_r, Err):
-        return out_err_r
+        return BErr("`git rev-parse` command failed", cause=out_err_r.err())
     else:
         out, _err = out_err_r.ok()
         return Ok(out)
@@ -30,7 +30,7 @@ def remotes() -> BResult[List[GitRemote]]:
     """Python wrapper around the `git remote -v` command."""
     out_err_r = bsp.safe_popen(["git", "remote", "-v"])
     if isinstance(out_err_r, Err):
-        return out_err_r
+        return BErr("`git remote` command failed", cause=out_err_r.err())
 
     out, _err = out_err_r.ok()
 
@@ -51,15 +51,18 @@ def local_branch_exists(branch: str) -> BResult[bool]:
     return _branch_exists(["git", "branch", "--list", branch])
 
 
-def remote_branch_exists(remote, branch):
-    # type: (str, str) -> BResult[bool]
+def remote_branch_exists(remote: str, branch: str) -> BResult[bool]:
     return _branch_exists(["git", "ls-remote", "--heads", remote, branch])
 
 
 def _branch_exists(cmd_parts: Iterable[str]) -> BResult[bool]:
     out_err_r = bsp.safe_popen(list(cmd_parts))
     if isinstance(out_err_r, Err):
-        return out_err_r
+        return BErr(
+            "The git command that was supposed to determine whether or not a"
+            " git branch exists has failed.",
+            cause=out_err_r.err(),
+        )
     else:
         out, _err = out_err_r.ok()
         return Ok(bool(out))
@@ -72,9 +75,8 @@ def checkout(branch: str, template_branch: str = None) -> BResult[None]:
     else:
         cmd_list.extend(["-b", branch, template_branch])
 
-    r = bsp.safe_popen(cmd_list, stdout=sys.stdout)
-    if isinstance(r, Err):
-        return r
+    if error := bsp.safe_popen(cmd_list, stdout=sys.stdout).err():
+        return BErr("The `git checkout` command has failed.", cause=error)
 
     return Ok(None)
 
@@ -99,9 +101,10 @@ def add_remote(name: str, url: str) -> BResult[None]:
 def _git_cmd(cmd: str, *opts: str) -> BResult[None]:
     cmd_list = ["git", cmd]
     cmd_list.extend(opts)
-    r = bsp.safe_popen(cmd_list, stdout=sys.stdout)
-    if isinstance(r, Err):
-        return r
+    if error := bsp.safe_popen(cmd_list, stdout=sys.stdout).err():
+        return BErr(
+            f"The following git command has failed: {cmd_list!r}", cause=error
+        )
 
     return Ok(None)
 
@@ -109,7 +112,9 @@ def _git_cmd(cmd: str, *opts: str) -> BResult[None]:
 def current_branch() -> BResult[str]:
     ref_r = bsp.safe_popen(["git", "symbolic-ref", "--quiet", "HEAD"])
     if isinstance(ref_r, Err):
-        return ref_r
+        return BErr(
+            "The `git symbolic-ref` command has failed.", cause=ref_r.err()
+        )
 
     ref, _ = ref_r.ok()
     return Ok(ref.split("/")[-1])
